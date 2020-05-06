a6e58350b047b225efe3280596d319c2da516908
Cordova Serrano Eduardo Javier <60123447+svmpler@users.noreply.github.com>
2020-05-06 17:54:16 -0500

    First commit

---
+++ Inicio.java
@@ -0,0 +1,30 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Inicio here.
+ * Pantalla principal del videojuego
+ * @author (Eduardo Javier Córdova Serrano) 
+ * @version (Martes 21 de Abril del 2020)
+ */
+public class Inicio extends World
+{
+
+    /**
+     * Constructor for objects of class Inicio.
+     * 
+     */
+    public Inicio()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(1000, 800, 1); 
+        prepare();
+    }
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+    }
+}
---
+++ doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Inicio","l":"Inicio()","url":"%3Cinit%3E()"}]
