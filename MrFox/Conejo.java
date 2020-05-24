e1fa91fab8a0b355139577f493205c591be38404
Aczayacatl BS <52363947+Aczayacatl@users.noreply.github.com>
2020-05-24 10:28:22 -0500

    A la clase "Conejo" le impremente la animacion usando la clase "FrameContainer". Tambien le hice una pequeña IA a la clase "Conejo" (de IA no tiene nada, solo le dije al conejo que se moviera en direcciones aleatorias cuando aparesca)

---
+++ MrFox/Conejo.java
@@ -0,0 +1,79 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.*;
+/**
+ * Write a description of class Conejo here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Conejo extends Actor
+{
+    private String[] framesWalkDer = {"ConejoWalk/conejoWalk1R.png","ConejoWalk/conejoWalk2R.png","ConejoWalk/conejoWalk3R.png","ConejoWalk/conejoWalk4R.png","ConejoWalk/conejoWalk5R.png"};
+    private String[] framesWalkIzq = {"ConejoWalk/conejoWalk1L.png","ConejoWalk/conejoWalk2L.png","ConejoWalk/conejoWalk3L.png","ConejoWalk/conejoWalk4L.png","ConejoWalk/conejoWalk5L.png"};
+        
+    private int velocity = 10;
+    private String key;
+    private FrameContainer frameContainer;
+    
+    /*---Variables usadas para direcciones random---*/
+    private Random DireccionConejo;
+    private int DireccionConejoX;
+    private int DireccionConejoY;
+    private int contadorParaCambiarDireccion=0;
+    
+    public Conejo(){
+        frameContainer = new FrameContainer();
+        DireccionConejo = new Random();
+    }
+    
+    public void act() 
+    {
+        IAconejo();
+        animacionConFrameContainer();
+    }    
+    
+    public void IAconejo(){
+        key = Greenfoot.getKey();
+        int x = getX();
+        int y = getY();
+        
+        /*---IA---*/
+        if(contadorParaCambiarDireccion==0){
+            DireccionConejoX = DireccionConejo.nextInt(2); //Numero aleatorio entre 0 y 1
+            DireccionConejoY = DireccionConejo.nextInt(2); //Numero aleatorio entre 0 y 1
+            contadorParaCambiarDireccion=15;
+        }
+        else{
+            contadorParaCambiarDireccion--;
+        }
+        if(DireccionConejoX==0){
+            x = x - velocity;
+        }
+        if(DireccionConejoX==1){
+            x = x + velocity;
+        }
+        if(DireccionConejoY==0){
+            y = y - velocity;
+        }
+        if(DireccionConejoY==1){
+            y = y + velocity;
+        }
+        /*--- Controles de frameStill ---*/
+        if(key==null){
+            frameContainer.setFrameStill(0);
+        } else {
+            frameContainer.setFrameStill(1);
+        }
+        
+        setLocation(x,y);
+        
+    }
+    
+    public void animacionConFrameContainer(){
+        if(DireccionConejoX==1) {
+            frameContainer.animacion(framesWalkDer, this);
+        } else {
+            frameContainer.animacion(framesWalkIzq, this);
+        }
+    }
+}
